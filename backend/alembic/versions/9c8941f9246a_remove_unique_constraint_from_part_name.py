c77d1651a23447fb2c79611dda4e791ba7b46d1b
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2026-07-16 12:15:44 +0200

    style: format alembic migration file with black

--- backend/alembic/versions/9c8941f9246a_remove_unique_constraint_from_part_name.py
+++ backend/alembic/versions/9c8941f9246a_remove_unique_constraint_from_part_name.py
@@ -10,7 +10,6 @@ from typing import Sequence, Union
 
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
 revision: str = "9c8941f9246a"
 down_revision: Union[str, Sequence[str], None] = "5875d9cb6f71"
