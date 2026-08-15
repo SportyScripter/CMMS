274a40defaef03cc19fea141aea08a36056a17c1
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2026-08-15 11:29:13 +0200

    chore: balck formater

--- backend/alembic/versions/976576caa33b_add_connection_between_departmant_and_.py
+++ backend/alembic/versions/976576caa33b_add_connection_between_departmant_and_.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "976576caa33b"
 down_revision: Union[str, Sequence[str], None] = "2681150802e8"
