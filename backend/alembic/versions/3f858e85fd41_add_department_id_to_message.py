9379319cf62b5c6852445b2b6388e2c35b53bcb0
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2026-08-18 11:19:56 +0200

    style(backend): apply black code formatting

--- backend/alembic/versions/3f858e85fd41_add_department_id_to_message.py
+++ backend/alembic/versions/3f858e85fd41_add_department_id_to_message.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "3f858e85fd41"
 down_revision: Union[str, Sequence[str], None] = "976576caa33b"
